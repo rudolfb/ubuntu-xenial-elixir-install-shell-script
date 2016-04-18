2e12df6218281ba1f00dbe5ff277c55b1b4aaa2c
Rudolf Bargholz <rudolf@bargholz.ch>
2016-04-18 16:53:24 +0200

    Refactoring of  script
    Moved common code to shell functions
    Added code that echos the commands: set -x / set +x
    Moved ENV variables to the top of the file
    Added config options for git
    Uniform indentation
    Postgres service restart  must be performed as superuser
    Moved emacs to start of script
    emacs opens in the background during the installation so that it can
    download necessary packages in parallel in the background

--- install.sh
+++ install.sh
@@ -1,19 +1,64 @@
-*+-usr-bin-env bash
+#!/bin/bash
 
-echo on
+section_heading () {
+    echo " "
+    echo " "
+    echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
+    echo " $1"
+    if [ -n "$2" ]; then
+        # $2 is set to a non-empty string
+        echo " $2"
+    fi
+    echo "======================================================================"
+}
 
+sub_section_heading () {
+    echo " "
+    echo " "
+    echo "--- $1"
+    echo "--------------------"
+}
+
+
+# ----------------------------------------------------------------------------------------------------
+section_heading "Environment variables"
+# ----------------------------------------------------------------------------------------------------
+# Env variables used in the commands below
+
+sub_section_heading "postgres"
+set -x
+# Edit the following to change the name of the database user that will be created:
+POSTGRES_APP_DB_USER=postgres
+POSTGRES_APP_DB_PASS=$POSTGRES_APP_DB_USER
+
+# Edit the following to change the name of the database that is created (defaults to the user name)
+# POSTGRES_APP_DB_NAME=${POSTGRES_APP_DB_USER}_unused
+
+# Edit the following to change the version of PostgreSQL that is installed
+PG_VERSION=9.5
+set +x
+
+sub_section_heading "git"
+set -x
+GIT_USER_EMAIL=""
+GIT_USER_NAME=""
+GIT_CREDENTIAL_HELPER_CACHE_TIMEOUT=86400
+set +x
+
+
+# ----------------------------------------------------------------------------------------------------
+section_heading "Dependencies"
+# ----------------------------------------------------------------------------------------------------
+set -x
 cd ~/Downloads/
 mkdir autoinstall
 cd autoinstall
-
-echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
-echo "| Dependencies                                                        |"
-echo "======================================================================"
 
 # For atom
 sudo add-apt-repository -y ppa:webupd8team/atom
 
 sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev git
 # libgtk2.0-0 libgtk2.0-bin libgtk2.0-common
 
@@ -30,146 +75,18 @@ wget http://archive.ubuntu.com/ubuntu/pool/universe/w/wxwidgets3.0/libwxgtk3.0-0
 yes Y | sudo dpkg -i libwxgtk3.0-0_3.0.2-1*.deb
 yes Y | sudo apt-get -fy install
 
-echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
-echo "| nodejs 5.x                                                         |"
-echo "======================================================================"
 
-curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
-echo "| postgres and pgadmin                                               |"
-echo "======================================================================"
-
-# Edit the following to change the name of the database user that will be created:
-APP_DB_USER=postgres
-APP_DB_PASS=$APP_DB_USER
-
-# Edit the following to change the name of the database that is created (defaults to the user name)
-# APP_DB_NAME=${APP_DB_USER}_unused
-
-# Edit the following to change the version of PostgreSQL that is installed
-PG_VERSION=9.5
-
-###########################################################
-# Changes below this line are probably not necessary
-###########################################################
-print_db_usage () {
-  echo "Your PostgreSQL database has been setup and can be accessed on your local machine on the forwarded port (default: 5432)"
-  echo "  Host: localhost"
-  echo "  Port: 5432"
-  echo "  Database: <DATABASE_NAME>"
-  echo "  Username: $APP_DB_USER"
-  echo "  Password: $APP_DB_PASS"
-  echo ""
-  echo "Admin access to postgres user via VM:"
-  echo "  vagrant ssh"
-  echo "  sudo su - postgres"
-  echo ""
-  echo "psql access to app database user via VM:"
-  echo "  vagrant ssh"
-  echo "  sudo su - postgres"
-  echo "  PGUSER=$APP_DB_USER PGPASSWORD=$APP_DB_PASS psql -h localhost <DATABASE_NAME>"
-  echo ""
-  echo "Env variable for application development:"
-  echo "  DATABASE_URL=postgresql://$APP_DB_USER:$APP_DB_PASS@localhost:5432/<DATABASE_NAME>"
-  echo ""
-  echo "Local command to access the database via psql:"
-  echo "  PGUSER=$APP_DB_USER PGPASSWORD=$APP_DB_PASS psql -h localhost -p 5432 <DATABASE_NAME>"
-}
-
-PG_REPO_APT_SOURCE=/etc/apt/sources.list.d/pgdg.list
-if [ ! -f "$PG_REPO_APT_SOURCE" ]
-then
-  # Add PG apt repo:
-  echo "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main" > "$PG_REPO_APT_SOURCE"
-
-  # Add PGDG repo key:
-  wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
-fi
-
-# Update package list and upgrade all packages
-sudo apt-get update
-sudo apt-get -y upgrade
-
-sudo apt-get -y install "postgresql-$PG_VERSION" "postgresql-contrib-$PG_VERSION"
-sudo apt-get -y install libpq-dev # For building ruby 'pg' gem
-sudo apt-get -y install pgadmin3
-
-PG_CONF="/etc/postgresql/$PG_VERSION/main/postgresql.conf"
-PG_HBA="/etc/postgresql/$PG_VERSION/main/pg_hba.conf"
-PG_DIR="/var/lib/postgresql/$PG_VERSION/main"
-
-# Edit postgresql.conf to change listen address to '*':
-sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" "$PG_CONF"
-
-# Append to pg_hba.conf to add password auth:
-echo "host    all             all             all                     md5" >> "$PG_HBA"
-
-# Explicitly set default client_encoding
-echo "client_encoding = utf8" >> "$PG_CONF"
-
-# Restart so that all new config is loaded:
-service postgresql restart
-
-cat << EOF | su - postgres -c psql
--- Create the database user:
-CREATE USER $APP_DB_USER PASSWORD '$APP_DB_PASS' CREATEDB;
-EOF
-
-# cat << EOF | su - postgres -c psql
-# -- Create the database:
-# CREATE DATABASE $APP_DB_NAME WITH OWNER=$APP_DB_USER
-#                                   LC_COLLATE='en_US.utf8'
-#                                   LC_CTYPE='en_US.utf8'
-#                                   ENCODING='UTF8'
-#                                   TEMPLATE=template0;
-# EOF
-
-# Tag the provision time:
-
-echo "Successfully created PostgreSQL dev virtual machine."
-echo ""
-print_db_usage
-
-echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
-echo "| elixir, incl. erlang                                               |"
-echo "======================================================================"
-
-wget http://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc
-sudo apt-key add erlang_solutions.asc
-# There is as of now no erlang download for xenial, so we have to hard code wily.
-sudo add-apt-repository "deb http://packages.erlang-solutions.com/ubuntu wily contrib"
-# sudo add-apt-repository "deb http://packages.erlang-solutions.com/ubuntu $(lsb_release -s -c) contrib"
-sudo apt-get update
-yes Y | sudo apt-get -y install esl-erlang
-sudo apt-get -y install elixir
-
-echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
-echo "| phoenix                                                            |"
-echo "======================================================================"
-
-mix local.hex --force
-mix local.rebar --force
-mix archive.install "https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez" --force
-
-echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
-echo "| elm                                                                |"
-echo "======================================================================"
-
-sudo npm install -g elm
-
-echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
-echo "| emacs                                                              |"
-echo "======================================================================"
-
+set +x
+# ----------------------------------------------------------------------------------------------------
+section_heading "emacs"
+# ----------------------------------------------------------------------------------------------------
+set -x
 sudo apt-get -y install emacs
 cd ~
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 # Revert back to previous directory
 wget https://github.com/rudolfb/ubuntu-xenial-elixir-install-shell-script/raw/master/.spacemacs
 cd -
-
 
 # http://www.zohaib.me/spacemacs-and-alchemist-to-make-elixir-of-immortality/
 # Need to ensure the dotspacemacs-configuration-layers looks similar to the following:
@@ -196,105 +113,201 @@ cd -
 # themes-megapack
 # ;; version-control
 
-
 # Then restart emacs
 
-echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
-echo "| atom                                                                |"
-echo "======================================================================"
+# Start emacs minimized so that while everything else is being 
+# installed the emacs packages can be downloaded in the background.
+emacs --iconic &
 
+set +x
+# ----------------------------------------------------------------------------------------------------
+section_heading "git"
+# ----------------------------------------------------------------------------------------------------
+set -x
+if [ -n "${GIT_USER_EMAIL}" ]; then
+    # "VAR is set to a non-empty string"
+    git config --global user.email "$GIT_USER_EMAIL"
+fi
+if [ -n "${GIT_USER_EMAIL}" ]; then
+    git config --global user.name "$GIT_USER_NAME"
+fi
+
+if [ -n "${GIT_CREDENTIAL_HELPER_CACHE_TIMEOUT}" ]; then
+    # Save your password in memory and set the cache time in minutes manually:
+    git config --global credential.helper 'cache --timeout=$GIT_CREDENTIAL_HELPER_CACHE_TIMEOUT'
+    # Set the cache to timeout after x secconds
+    # 86400 = 1 day
+    # default is 15 minutes
+fi
+
+
+set +x
+# ----------------------------------------------------------------------------------------------------
+section_heading "nodejs 5.x"
+# ----------------------------------------------------------------------------------------------------
+set -x
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+set +x
+echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
+echo "| postgres and pgadmin                                               |"
+echo "======================================================================"
+set -x
+
+###########################################################
+# Changes below this line are probably not necessary
+###########################################################
+
+PG_REPO_APT_SOURCE=/etc/apt/sources.list.d/pgdg.list
+if [ ! -f "$PG_REPO_APT_SOURCE" ]
+then
+    # Add PG apt repo:
+    echo "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main" > "$PG_REPO_APT_SOURCE"
+    
+    # Add PGDG repo key:
+    wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+fi
+
+
+# Update package list and upgrade all packages
+sudo apt-get update
+sudo apt-get -y upgrade
+
+sudo apt-get -y install "postgresql-$PG_VERSION" "postgresql-contrib-$PG_VERSION"
+sudo apt-get -y install libpq-dev # For building ruby 'pg' gem
+sudo apt-get -y install pgadmin3
+
+PG_CONF="/etc/postgresql/$PG_VERSION/main/postgresql.conf"
+PG_HBA="/etc/postgresql/$PG_VERSION/main/pg_hba.conf"
+PG_DIR="/var/lib/postgresql/$PG_VERSION/main"
+
+# Edit postgresql.conf to change listen address to '*':
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" "$PG_CONF"
+
+# Append to pg_hba.conf to add password auth:
+sudo echo "host    all             all             all                     md5" >> "$PG_HBA"
+
+# Explicitly set default client_encoding
+sudo echo "client_encoding = utf8" >> "$PG_CONF"
+
+# Restart so that all new config is loaded:
+sudo service postgresql restart
+
+cat << EOF | su - postgres -c psql
+-- Create the database user:
+CREATE USER $POSTGRES_APP_DB_USER PASSWORD '$POSTGRES_APP_DB_PASS' CREATEDB;
+EOF
+
+# cat << EOF | su - postgres -c psql
+# -- Create the database:
+# CREATE DATABASE $POSTGRES_APP_DB_NAME WITH OWNER=$POSTGRES_APP_DB_USER
+#                                   LC_COLLATE='en_US.utf8'
+#                                   LC_CTYPE='en_US.utf8'
+#                                   ENCODING='UTF8'
+#                                   TEMPLATE=template0;
+# EOF
+
+# Tag the provision time:
+
+
+set +x
+# ----------------------------------------------------------------------------------------------------
+section_heading "elixir, incl. erlang"
+# ----------------------------------------------------------------------------------------------------
+set -x
+wget http://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc
+sudo apt-key add erlang_solutions.asc
+# There is as of now no erlang download for xenial, so we have to hard code wily.
+sudo add-apt-repository "deb http://packages.erlang-solutions.com/ubuntu wily contrib"
+# sudo add-apt-repository "deb http://packages.erlang-solutions.com/ubuntu $(lsb_release -s -c) contrib"
+sudo apt-get update
+yes Y | sudo apt-get -y install esl-erlang
+sudo apt-get -y install elixir
+
+
+set +x
+# ----------------------------------------------------------------------------------------------------
+section_heading "phoenix"
+# ----------------------------------------------------------------------------------------------------
+set -x
+mix local.hex --force
+mix local.rebar --force
+mix archive.install "https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez" --force
+
+
+set +x
+# ----------------------------------------------------------------------------------------------------
+section_heading "elm"
+# ----------------------------------------------------------------------------------------------------
+set -x
+sudo npm install -g elm
+
+
+set +x
+# ----------------------------------------------------------------------------------------------------
+section_heading "atom"
+# ----------------------------------------------------------------------------------------------------
+set -x
 # wget https://github.com/atom/atom/releases/download/v1.6.2/atom-amd64.deb
 # sudo dpkg --install atom-amd64.deb
 sudo apt-get -y install atom
 apm install  autocomplete-elixir elm-format file-icons git-plus html-to-elm language-elixir language-elm language-lisp linter linter-elixirc linter-xmllint merge-conflicts minimap project-manager refactor regex-railroad-diagram split-diff tabs-to-spaces trailing-spaces xml-formatter
 
-echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
-echo "| check the version numbers to test the installations                |"
-echo "======================================================================"
 
-su $USER
+set +x
+# ----------------------------------------------------------------------------------------------------
+section_heading "Check the version numbers to test the installations"
+# ----------------------------------------------------------------------------------------------------
 
-echo " "
-echo " "
-echo "--- git"
-echo "--------------------"
+sub_section_heading "git"
 echo "git --version"
 git --version
-echo " "
-echo " "
-echo "--- node"
-echo "--------------------"
+
+sub_section_heading "node"
 echo "node --version"
 node --version
-echo " "
-echo " "
-echo "--- postgres"
-echo "--------------------"
+
+sub_section_heading "postgres"
 echo "psql --version"
 psql --version
-echo " "
-echo " "
-echo "--- erlang"
-echo "--------------------"
+
+sub_section_heading "erlang"
 echo "cat /usr/lib/erlang/releases/RELEASES"
 cat /usr/lib/erlang/releases/RELEASES
-echo " "
-echo " "
-echo "--- elixir"
-echo "--------------------"
+
+sub_section_heading "elixir"
 echo "elixir --version"
 elixir --version
-echo " "
-echo " "
-echo "--- phoenix"
-echo "--------------------"
-echo "mix help phoenix.new"
+
+sub_section_heading "phoenix"
 if [ $(strings ~/.mix/archives/phoenix_new.ez | grep -co '{vsn,') = 0 ]; then
-  echo "Error: unable to find installed phoenix"
+    echo "Error: unable to find installed phoenix"
 else
-  mix help phoenix.new | sed -e 3b -e '$!d'
-  strings ~/.mix/archives/phoenix_new.ez | grep '{vsn,'
+    echo "mix help phoenix.new"
+    mix help phoenix.new | sed -e 3b -e '$!d'
+    strings ~/.mix/archives/phoenix_new.ez | grep '{vsn,'
 fi
-echo " "
-echo " "
-echo "--- elm"
-echo "--------------------"
-echo "elm"
+
+sub_section_heading "elm"
 if [ $(elm | grep -co 'Elm Platform') = 0 ]; then
-  echo "Error: unable to find installed elm"
+    echo "Error: unable to find installed elm"
 else
-  elm | sed -e 1b -e '$!d'
+    echo "elm"
+    elm | sed -e 1b -e '$!d'
 fi
-echo " "
-echo " "
-echo "--- emacs"
-echo "--------------------"
+
+sub_section_heading "emacs"
 echo "emacs --version"
 emacs --version
-echo " "
-echo " "
-echo "--- atom"
-echo "--------------------"
+echo "Enter 'emacs' in a shell to start emacs."
+
+sub_section_heading "atom"
 echo "atom --version"
 atom --version
-echo " "
-echo " "
-
-echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
-echo "| end                                                                |"
-echo "======================================================================"
-
-# Setting up git
-
-# git config --global user.email "you@example.com"
-# git config --global user.name "Your Name"
-
-# Save your password in memory for a default of 15 minutes:
-# git config --global credential.helper cache
-
-# Save your password in memory and set the cache time in minutes manually:
-# git config --global credential.helper 'cache --timeout=3600'
-# Set the cache to timeout after 1 hour (setting is in seconds)
+echo "Enter 'atom' in a shell to start atom."
 
 
-
+# ----------------------------------------------------------------------------------------------------
+section_heading "End"
+# ----------------------------------------------------------------------------------------------------
