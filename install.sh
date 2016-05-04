8cdba613fafabd7565047106d4efb53a2c3812c0
Rudolf Bargholz <rudolf@bargholz.ch>
2016-05-04 13:31:42 +0200

    Prevent script from pausing during Erlang/Elixir install

--- install.sh
+++ install.sh
@@ -211,8 +211,8 @@ main_section_heading "elixir, incl. erlang"
 set -x
 wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
 sudo apt-get update
-sudo apt-get install esl-erlang
-sudo apt-get install elixir
+sudo apt-get -y install esl-erlang
+sudo apt-get -y install elixir
 
 
 set +x
